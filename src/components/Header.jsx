bd385074e1a7626b5292320d15bbcd9ff0be3464
TheQuestioningCake <perrywayne39@gmail.com>
2024-07-03 13:41:23 -0400

    started on header.jsx, and added logic for the vite.config.js

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+    return(
+        <header>
+            <h1>Wayne Perry</h1>
+            <nav>
+
+            </nav>
+        </header>
+    );
+}
+
+export default Header
