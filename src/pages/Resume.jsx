4823b3990372c0560ba9598aba6d59b06fe557bf
TheQuestioningCake <perrywayne39@gmail.com>
2024-07-09 22:40:51 -0400

    debugging

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,7 +4,7 @@ function Resume() {
         <div className="container resume">
   <h2>Resume</h2>
   <h3>You can find a link to my resume down below, hope we end up scheduling an interview very soon</h3>
-  <div className="btn-center"> <a href="../public/assets/reactResume.pdf" className="hire-me"><i className="icon-download"></i> Download Resume</a>
+  <div className="btn-center"> <a href="assets/reactResume.pdf" className="hire-me"><i className="icon-download"></i> Download Resume</a>
   </div>
 </div>
         </>
