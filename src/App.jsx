0486d810cecb2be2f36132038dd4d38d6bad78a5
TheQuestioningCake <perrywayne39@gmail.com>
2024-07-10 08:24:14 -0400

    deleting unecessary files

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
-// import './App.css'
 import Footer from './components/Footer'
 import Navigation from './components/Navigation'
-// import Socials from './components/Socials'
-// import Main from './components/Main'
-// import Aside from './components/Aside'
 import { Outlet } from 'react-router-dom'
 function App() {
     return (
         <>
             <Navigation />
             <Outlet />
-            {/* <Main  />
-            <Aside /> */}
-            {/* <Socials /> */}
                 <Footer />
         </>
     );
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,7 +3,7 @@ function Project({ work }) {
         <>
             <li>
                 <div>
-                    {/* <h3>{work.title}</h3> */}
+                    <h3>{work.title}</h3>
                     <a className="fancybox-thumb" rel="fancybox-thumb" style={{width:"100%"}} href={work.github} title="Image 01"><img src={work.img} style={{ width: "100%" }} alt="" /></a></div>
             </li>
         </>
